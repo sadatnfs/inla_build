facc9e0759379a31b3521e34913523661af3d5c8
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 12:38:03 -0700

    Add sudo to wget

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-wget https://gist.githubusercontent.com/sadatnfs/8c28a1024fbec239fa29347bea456918/raw/979ab801d43b8cd63a2c8a063578619987f0e728/Makefile
+sudo wget https://gist.githubusercontent.com/sadatnfs/8c28a1024fbec239fa29347bea456918/raw/979ab801d43b8cd63a2c8a063578619987f0e728/Makefile
 sudo make lib-install
 sudo make download
