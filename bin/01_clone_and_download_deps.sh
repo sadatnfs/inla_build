04b3b0417c36d5c62b235d2e224109510908640a
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 12:54:08 -0700

    SPACING???

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/ad13d08094d84d0c1e6c24966fdcabe4fe466991/Makefile
+sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/233849deebc80c3cff3e053bcc57afcfe3a22258/Makefile
 sudo make lib-install
 sudo make download
