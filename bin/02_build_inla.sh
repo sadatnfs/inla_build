4e5ac2d7cb640c2d0e635c2a021b9f83eaa7d596
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 12:44:00 -0700

    Apparently I was pushing bad 02

--- bin/02_build_inla.sh
+++ bin/02_build_inla.sh
@@ -3,13 +3,13 @@ set -e # Exit with nonzero exit code if anything fails
 
 
 ## Clone INLA
-cd /home/travis/R-bin/lib/R/library
+cd /usr/lib/R/library
 sudo make bin
 sudo mv inla/rinla/R/obsolete/* inla/rinla/R/ 
 sudo make package 
-R --vanilla CMD INSTALL /home/travis/R-bin/lib/R/library/tmp/RINLA/INLA_*.tgz 
-sudo ln -s /home/travis/R-bin/lib/R/library/INLA/bin/linux/64bit/fmesher.run /home/travis/R-bin/lib/R/library/INLA/bin/linux/64bit/fmesher.static 
-sudo ln -s /home/travis/R-bin/lib/R/library/INLA/bin/linux/32bit/fmesher.run /home/travis/R-bin/lib/R/library/INLA/bin/linux/32bit/fmesher.static 
-sudo ln -s /home/travis/R-bin/lib/R/library/INLA/bin/linux/64bit/inla.run /home/travis/R-bin/lib/R/library/INLA/bin/linux/64bit/inla.static 
-sudo ln -s /home/travis/R-bin/lib/R/library/INLA/bin/linux/32bit/inla.run /home/travis/R-bin/lib/R/library/INLA/bin/linux/32bit/inla.static
+R --vanilla CMD INSTALL /usr/lib/R/library/tmp/RINLA/INLA_*.tgz 
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.static 
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.static 
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/inla.run /usr/lib/R/library/INLA/bin/linux/64bit/inla.static 
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/inla.run /usr/lib/R/library/INLA/bin/linux/32bit/inla.static
 
