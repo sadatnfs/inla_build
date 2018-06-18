f3b54139ce3bd34be0f898c13153d0578721a983
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 11:53:17 -0700

    Changing all L and I flags to use travis R-bin

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /home/travis/R-bin/lib/R/library
-wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/01bc3ec3af5e3e82cce7d54e829b729837726950/Makefile
+wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/2e7fe96065c3ab5596c25279eaa6ca4b7b129961/Makefile
 sudo make lib-install
 sudo make download -d 
