5043db70a03a7b8c8650154bac6e13139604bd58
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 12:57:40 -0700

    Remove INLA_LIBR altogether

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/233849deebc80c3cff3e053bcc57afcfe3a22258/Makefile
+sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/ebb7be578e46b65eec544574fba7ac4ef07973aa/Makefile
 sudo make lib-install
 sudo make download
