b8af2a4ef1641bf3f4c60ad50acec468be6c764d
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 12:02:29 -0700

    One more white space...

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /home/travis/R-bin/lib/R/library
-wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/d685dc0db5ad7c60281a3bc041864a0ee66d0480/Makefile
+wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/5654de02b1778c35ed2594b123dc01434e31f101/Makefile
 sudo make lib-install
 sudo make download -d 
