6f327f38e7880ceb4f7f9ad11bcbc722234cbfaa
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 13:00:15 -0700

    NOOOO NOT A SPACE HANGIN

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/ebb7be578e46b65eec544574fba7ac4ef07973aa/Makefile
+sudo wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/faedf15f2354a9d481881b8b33147dcd944ca0de/Makefile
 sudo make lib-install
 sudo make download
