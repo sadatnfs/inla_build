960fe8fe7ae5f9a7749e68dfda82e4a62788a65a
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 11:43:20 -0700

    Update Makefile to not have all those extra flags

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /home/travis/R-bin/lib/R/library
-wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/2167a0ec0c77008fe5955e62ff25a01a2ae6e08f/Makefile
+wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/01bc3ec3af5e3e82cce7d54e829b729837726950/Makefile
 sudo make lib-install
 sudo make download -d 
