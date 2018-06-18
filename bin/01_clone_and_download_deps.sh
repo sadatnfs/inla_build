a6060c90f7d999a1fbeb62878e9f39d94a193939
Nafis Sadat <sadatnfs@uw.edu>
2018-06-18 11:56:36 -0700

    Make sux

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -4,6 +4,6 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /home/travis/R-bin/lib/R/library
-wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/2e7fe96065c3ab5596c25279eaa6ca4b7b129961/Makefile
+wget https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/d685dc0db5ad7c60281a3bc041864a0ee66d0480/Makefile
 sudo make lib-install
 sudo make download -d 
