ddec8ff4255339c3a2e6b13ebb33ee9f987870e6
Nafis Sadat <sadatnfs@uw.edu>
2018-06-19 09:56:21 -0700

    Add back sudo..

--- bin/02_build_inla.sh
+++ bin/02_build_inla.sh
@@ -4,10 +4,10 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-make lib-install && make bin && mv inla/rinla/R/obsolete/* inla/rinla/R/ && make package
+sudo make lib-install && sudo make bin && mv inla/rinla/R/obsolete/* inla/rinla/R/ && sudo make package
 R --vanilla CMD INSTALL /usr/lib/R/library/tmp/RINLA/INLA_*.tgz
-ln -s /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.static 
-ln -s /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.static 
-ln -s /usr/lib/R/library/INLA/bin/linux/64bit/inla.run /usr/lib/R/library/INLA/bin/linux/64bit/inla.static 
-ln -s /usr/lib/R/library/INLA/bin/linux/32bit/inla.run /usr/lib/R/library/INLA/bin/linux/32bit/inla.static
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.static
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.static
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/inla.run /usr/lib/R/library/INLA/bin/linux/64bit/inla.static
+sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/inla.run /usr/lib/R/library/INLA/bin/linux/32bit/inla.static
 
