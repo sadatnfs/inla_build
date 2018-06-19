a6d2c8bd78e0132af9c415df72db3f64ddbf757d
Nafis Sadat <sadatnfs@uw.edu>
2018-06-19 08:46:06 -0700

    Added explicit gsl pointers and librso

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -5,6 +5,6 @@ set -e # Exit with nonzero exit code if anything fails
 ## Clone INLA
 cd /usr/lib/R/library
 # sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/faedf15f2354a9d481881b8b33147dcd944ca0de/Makefile
-sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/b0a7b39e2fc6fa5840e2c05087931fa3ad3e183f/Makefile
+sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/f4a8b4e51b746d1328d03e4155eb6df24fbb26f0/Makefile
 sudo make lib-install
 sudo make download
