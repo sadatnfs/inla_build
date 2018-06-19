f2aef058531515f0ba996e1ecdfdd2c386f1b3ba
Nafis Sadat <sadatnfs@uw.edu>
2018-06-19 08:33:28 -0700

    Add -y flag to apt install

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -5,6 +5,6 @@ set -e # Exit with nonzero exit code if anything fails
 ## Clone INLA
 cd /usr/lib/R/library
 # sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/faedf15f2354a9d481881b8b33147dcd944ca0de/Makefile
-sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/7fbd3b0d36c5ad472e6e0dcb47496344f1ce7fa9/Makefile
+sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/45807fd7db1f241aa947dc6cbb2f81e59c7efd0a/Makefile
 sudo make lib-install
 sudo make download
