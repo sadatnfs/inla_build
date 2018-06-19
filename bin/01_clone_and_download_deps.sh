2b9d734536233865df16104536995978fcd7b3ba
Nafis Sadat <sadatnfs@uw.edu>
2018-06-19 09:49:50 -0700

    Remove sudo and chain make commands in 02

--- bin/01_clone_and_download_deps.sh
+++ bin/01_clone_and_download_deps.sh
@@ -5,5 +5,5 @@ set -e # Exit with nonzero exit code if anything fails
 ## Clone INLA
 cd /usr/lib/R/library
 # sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/faedf15f2354a9d481881b8b33147dcd944ca0de/Makefile
-sudo wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/56959aa3de734f9e86a28afc39cb82114a5917b9/Makefile
-sudo make download
+wget --no-check-certificate https://gist.githubusercontent.com/sadatnfs/e8a0ca5fe821fb4d95b2b944face8fb3/raw/56959aa3de734f9e86a28afc39cb82114a5917b9/Makefile
+make download
--- bin/02_build_inla.sh
+++ bin/02_build_inla.sh
@@ -4,13 +4,10 @@ set -e # Exit with nonzero exit code if anything fails
 
 ## Clone INLA
 cd /usr/lib/R/library
-sudo make lib-install
-sudo make bin
-sudo mv inla/rinla/R/obsolete/* inla/rinla/R/
-sudo make package
+make lib-install && make bin && mv inla/rinla/R/obsolete/* inla/rinla/R/ && make package
 R --vanilla CMD INSTALL /usr/lib/R/library/tmp/RINLA/INLA_*.tgz
-sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.static 
-sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.static 
-sudo ln -s /usr/lib/R/library/INLA/bin/linux/64bit/inla.run /usr/lib/R/library/INLA/bin/linux/64bit/inla.static 
-sudo ln -s /usr/lib/R/library/INLA/bin/linux/32bit/inla.run /usr/lib/R/library/INLA/bin/linux/32bit/inla.static
+ln -s /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/64bit/fmesher.static 
+ln -s /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.run /usr/lib/R/library/INLA/bin/linux/32bit/fmesher.static 
+ln -s /usr/lib/R/library/INLA/bin/linux/64bit/inla.run /usr/lib/R/library/INLA/bin/linux/64bit/inla.static 
+ln -s /usr/lib/R/library/INLA/bin/linux/32bit/inla.run /usr/lib/R/library/INLA/bin/linux/32bit/inla.static
 
